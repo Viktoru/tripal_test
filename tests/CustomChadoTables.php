0ba8d768c33a4b8d9e31c2ecd829bfafc9a41bee
Viktoru <llajta9@gmail.com>
2017-02-28 15:29:31 -0800

    added CustomChadoTable sample

--- tests/CustomChadoTables.php
+++ tests/CustomChadoTables.php
@@ -1,1 +1,27 @@
+<?php
 
+// Bootstrap Drupal.
+define('DRUPAL_ROOT', '/var/www/html');
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+/**
+ *  PHPUnit Tests the testing the Custom Chado Tables.
+ *  Provides an API manage custom tables in Chado.
+ *
+ */
+class CustomChadoTables extends PHPUnit_Framework_TestCase {
+
+  public function test_chado_create_custom_table(){
+
+    // Create a table name.
+    $table_01 = 'table_1';
+    db_create_table($table_01, 'chado');
+    chado_create_custom_table($table_01, 'chado', $skip_if_exist = 1, $mview_id = NULL);
+
+    var_dump($table_01);
+
+  }
+
+}
